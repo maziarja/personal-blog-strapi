d29334f88882e87fb3840d83d18a06d0b80e09b8
maziarja <maziar.ja@gmail.com>
2026-03-15 14:29:17 -0700

    install postgres driver

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -472,6 +472,7 @@ export interface ApiBlogBlog extends Struct.CollectionTypeSchema {
   };
   attributes: {
     content: Schema.Attribute.RichText;
+    contentBlock: Schema.Attribute.Blocks;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
